43f6158773310e2790388fa34f2505be7cb0b6f7
Lisa Kohls <lk210@hdm-stuttgart.de>
2023-04-16 16:11:06 +0200

    React Navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,33 @@
+import { FaBars, FaTimes } from "react-icons/fa"
+import { useRef } from "react"
+import "../Styles/Main.css";
+
+function Navbar() {
+    const navRef = useRef();
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+    return (
+        <header>
+            <h3>logo</h3>
+            <nav ref={navRef}>
+                <a href="/#">Über uns</a>
+                <a href="/#">Reservierung</a>
+                <a href="/#">Produkte&Partner</a>
+                <a href="/#">Volunteers</a>
+                <a href="/#">Events</a>
+                <a href="/#">Kontakt</a>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars />
+            </button>
+
+        </header>
+    );
+
+}
+
+export default Navbar;
