9876077895408c0a3e363691f33e1df4182bbb99
Lisa Kohls <lk210@hdm-stuttgart.de>
2023-04-19 10:06:18 +0200

    app.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App'
 import './index.css';
-import { oeffnungszeiten } from './Resources/textInhalte'
 import reportWebVitals from './reportWebVitals';
-import Navbar from './Components/Navbar';
-import Button from './Components/Button';
-import Oeffnungszeiten from './Components/Oeffnungszeiten'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.Fragment>
-        <Navbar />
-    </React.Fragment>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-const btnÖffnungszeiten = ReactDOM.createRoot(document.querySelector('.btnÖffnungszeiten'));
-btnÖffnungszeiten.render(<Button name="Öffnungszeiten" />);
-
-const btnVolunteers = ReactDOM.createRoot(document.querySelector('.btnVolunteers'));
-btnVolunteers.render(<Button name="Volunteer" />);
-
-const btnReservierung = ReactDOM.createRoot(document.querySelector('.btnReservation'));
-btnReservierung.render(<Button name="Reservierung" />);
-
-const oeffnungszeitenBlock = ReactDOM.createRoot(document.querySelector('.oeffnungszeitenBlock'));
-oeffnungszeitenBlock.render(<Oeffnungszeiten inhalt={oeffnungszeiten} />);
 
 reportWebVitals();
