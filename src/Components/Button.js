505373c8efb26e838adc88695e1f1c9fb2d0b4d1
Lisa Kohls <lk210@hdm-stuttgart.de>
2023-04-26 11:15:27 +0200

    feat(oeffnungszeiten): anpassung an desktop version

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -4,9 +4,10 @@ import "../Styles/Button.css";
 function Button(props){
 
     const jumpTo = () => {
-        const anchor = document.getElementById(props.name)
-        window.scrollTo({top: anchor, behavior: 'smooth'});
+        //const anchor = document.getElementById(props.name)
+        window.location.href = `#${props.name}`;
     }
-    return (<button className="btn" onClick={jumpTo}  > {props.name} </button>);
+    return (<button className="btn" onClick={jumpTo} >
+        {props.name} </button>);
 }
 export default Button;
