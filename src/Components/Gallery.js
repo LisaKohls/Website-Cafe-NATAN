f28c4b0cf9792c2a0604b46dc181f60e99f5f3f7
lisasalzer <ls228@hdm-stuttgart.de>
2023-04-28 13:44:45 +0200

    footer changes, new component Gallery, added Contacts to Readme, added Menu card

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,12 +1,16 @@
 import  React from "react";
 import "../Styles/Gallery.css"
+import Img1 from '../Resources/Bildschirmfoto 2023-04-28 um 12.31.48.png'
+import Img2 from '../Resources/Bildschirmfoto 2023-04-28 um 12.32.05.png'
+import Img3 from '../Resources/Bildschirmfoto 2023-04-28 um 12.32.24.png'
+import Img4 from '../Resources/Bildschirmfoto 2023-04-28 um 12.32.37.png'
 
 function Gallery() {
     const images = [
-        { src: './Resources/Bildschirmfoto 2023-04-28 um 12.31.48.png', alt: 'Image 1' },
-        { src: './Ressources/Bildschirmfoto 2023-04-28 um 12.32.05.png', alt: 'Image 2' },
-        { src: './Ressources/Bildschirmfoto 2023-04-28 um 12.32.24.png', alt: 'Image 3' },
-        { src: './Ressources/Bildschirmfoto 2023-04-28 um 12.32.37.png', alt: 'Image 3' },
+        { src: Img1, alt: 'Image 1' },
+        { src: Img2, alt: 'Image 2' },
+        { src: Img3, alt: 'Image 3' },
+        { src: Img4, alt: 'Image 3' },
     ];
 
     return(
