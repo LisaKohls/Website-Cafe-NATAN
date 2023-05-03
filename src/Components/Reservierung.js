bc02d6ec839e2d636b7f7b260ad7fc11a41e7039
Lisa Kohls <lk210@hdm-stuttgart.de>
2023-05-03 11:23:41 +0200

    feat(reservierung):time picker und anordnung

--- src/Components/Reservierung.js
+++ src/Components/Reservierung.js
@@ -1,18 +1,28 @@
 import '../Styles/Reservierung.css'
 import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css";
+import DatePicker, { registerLocale } from 'react-datepicker';
+import de from 'date-fns/locale/de';
+import 'react-datepicker/dist/react-datepicker.css';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+import TimePicker from 'react-time-picker'
+import 'react-time-picker/dist/TimePicker.css';
 
 function Reservierung(){
     const [startDate, setStartDate] = useState(new Date());
+    const [value, onChange] = useState('10:00')
+    registerLocale("de", de)
     return(<div className='Datepicker'>
-            <label for='datePicker'>Wähle ein Datum: </label>
-            <DatePicker id='datePicker' selected={startDate} onChange={(date) => setStartDate(date)} />
+            <label for='datePicker' >Wähle ein Datum: </label>
+            <DatePicker id='datePicker' locale="de" selected={startDate} onChange={(date) => setStartDate(date)} dateFormat='dd MMMM yy'/>
+            <label for='timepicker'>Uhrzeit: </label>
+            <TimePicker id='timepicker' onChange={onChange} value={value} />
             <label for='name'>Name: </label>
             <input type='text' id='name' name='fname'></input>
+            <label for='personen'> Anzahl Personen</label>
+            <input type='number' id='personen' name='personen'></input>
             <label for='mail'>E-Mail: </label>
             <input type='text' id='mail' name='mail'></input>
+            <button className='submit'>Abschicken</button>
             </div>
     );
 }
