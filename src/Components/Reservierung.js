f4d3c0303783c48b17336bb81b825c5690dbb191
Lisa Kohls <lk210@hdm-stuttgart.de>
2023-05-04 16:44:16 +0200

    navigationsleiste gefixt Volunteer formular schöner gemacht

--- src/Components/Reservierung.js
+++ src/Components/Reservierung.js
@@ -1,21 +1,23 @@
 import '../Styles/Reservierung.css'
-import React, { useState } from 'react';
-import DatePicker, { registerLocale } from 'react-datepicker';
+import React, {useState} from 'react';
+import DatePicker, {registerLocale} from 'react-datepicker';
 import de from 'date-fns/locale/de';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import TimePicker from 'react-time-picker'
 import 'react-time-picker/dist/TimePicker.css';
 
-function Reservierung(){
+function Reservierung() {
     const [startDate, setStartDate] = useState(new Date());
     const [value, onChange] = useState('10:00')
     registerLocale("de", de)
-    return(<div className='Datepicker'>
-            <label for='datePicker' >Wähle ein Datum: </label>
-            <DatePicker id='datePicker' locale="de" selected={startDate} onChange={(date) => setStartDate(date)} dateFormat='dd MMMM yy'/>
+    return (
+        <div className='Datepicker'>
+            <label for='datePicker'>Wähle ein Datum: </label>
+            <DatePicker id='datePicker' locale="de" selected={startDate} onChange={(date) => setStartDate(date)}
+                        dateFormat='dd MMMM yy'/>
             <label for='timepicker'>Uhrzeit: </label>
-            <TimePicker id='timepicker' onChange={onChange} value={value} />
+            <TimePicker id='timepicker' onChange={onChange} value={value}/>
             <label for='name'>Name: </label>
             <input type='text' id='name' name='fname'></input>
             <label for='personen'> Anzahl Personen</label>
@@ -23,7 +25,7 @@ function Reservierung(){
             <label for='mail'>E-Mail: </label>
             <input type='text' id='mail' name='mail'></input>
             <button className='submit'>Abschicken</button>
-            </div>
+        </div>
     );
 }
 
--- src/Components/Volunteer.js
+++ src/Components/Volunteer.js
@@ -1,20 +1,28 @@
 import '../Styles/Volunteer.css'
 import '../Styles/Text.css'
 
-function Volunteer(){
-    return(
-        <div className='Volunteer'>
-        <form action="#" onSubmit="specialBonus(event)">
-            <h2>Als Volunteer bewerben</h2>
-            <p>Bitte fülle dieses Formular aus. Wir kontaktieren dich dann nach deiner Anfrage. </p>
-            <label htmlFor="name">Vor und Nachname</label>
-            <input type="text" id="name" name="name" required></input>
-            <label htmlFor="email">Mail</label>
-            <input type="email" id="email" name="mail" ></input>
-            <label htmlFor="textarea">Deine Nachricht an uns</label>
-            <textarea id="textarea"></textarea>
-            <button className='btnSubmit'>Abschicken</button>
-        </form>
+function Volunteer() {
+    return (
+        <div className='volunteer'>
+            <form action="#" onSubmit="specialBonus(event)">
+                <h2>Als Volunteer bewerben</h2>
+                <p>Bitte fülle dieses Formular aus. Wir kontaktieren dich dann nach deiner Anfrage. </p>
+                <div className="input-container">
+                    <div className="input-wrapper">
+                        <label htmlFor="name">Vor und Nachname</label>
+                        <input type="text" id="name" name="name" required></input>
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="email">Mail</label>
+                        <input type="email" id="email" name="mail"></input>
+                    </div>
+                    <div className="input-wrapper">
+                        <label htmlFor="textarea">Deine Nachricht an uns</label>
+                        <textarea id="textarea"></textarea>
+                    </div>
+                </div>
+                <button className='btnSubmit'>Abschicken</button>
+            </form>
         </div>
 
     )
