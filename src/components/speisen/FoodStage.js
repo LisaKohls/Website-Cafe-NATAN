bcc00ddf0d6762c17ef7dad25ecdf8b0e90fd03f
lisakohls <lk210@hdm-stuttgart.de>
2023-06-26 00:13:12 +0200

    Speisekarte faded in

--- src/components/speisen/FoodStage.js
+++ src/components/speisen/FoodStage.js
@@ -2,41 +2,23 @@ import React, {useEffect, useState} from 'react'
 import '../../styles/Food.css'
 import BlackFontText from '../text/BlackFontText'
 
-function FoodStage(props){
-    const [isVisible, setIsVisible] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const container = document.querySelector('.container');
-            const containerTop = container.getBoundingClientRect().top;
-            const windowHeight = window.innerHeight;
 
-            if (containerTop < windowHeight * 0.75) {
-                setIsVisible(true);
-            }
-            if (containerTop > windowHeight * 0.75) {
-                setIsVisible(false);
-            }
-        };
+function FoodStage(props) {
 
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-    return(
-        <div className={`container${isVisible ? ' visible' : ''}`}>
-        <div className="stage">
-            <div className="circle-wrapper">
-                <img className="circle-image" src={props.image} alt={props.name} />
+
+        return (
+            <div className='stage'>
+                <div className="circle-wrapper">
+                    <img className="circle-image" src={props.image} alt={props.name}/>
+                </div>
+                <div className="rectangle">
+                    <BlackFontText text={props.name} formatierung="centered"/>
+                    <BlackFontText text={props.description} formatierung="block"/>
+                </div>
             </div>
-            <div className='rectangle'>
-                <BlackFontText text={props.name} formatierung='centered'/>
-                <BlackFontText text={props.description} formatierung='block'/>
-            </div>
-        </div>
-</div>
-    )
+        );
+
+
 }
-
-export default FoodStage
+    export default FoodStage
--- src/components/speisen/SpecialFood.js
+++ src/components/speisen/SpecialFood.js
@@ -1,16 +1,44 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import FoodStage from "./FoodStage";
 import Food1 from '../../resources/specialFoodStaging/breakfast1.jpg'
 import Food2 from '../../resources/specialFoodStaging/drink2.jpg'
 import Food3 from '../../resources/specialFoodStaging/sweets3.jpg'
+import '../../styles/Food.css'
 import {description1, description2, description3, food1, food2, food3} from '../../resources/textInhalte'
 
 function SpecialFood(){
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleScroll = () => {
+        const stage = document.querySelector('.stage');
+        const stageTop = stage.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (stageTop < windowHeight  && stageTop > -stage.offsetHeight*0.5) {
+            setIsVisible(true);
+            console.log(isVisible)
+        } else {
+            setIsVisible(false);
+            console.log(isVisible)
+        }
+    };
+
+    useEffect(() => {
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return(
-        <div className='container'>
-            <FoodStage name={food1} image={Food1} description={description1}/>
-            <FoodStage name={food2} image={Food2} description={description2}/>
-            <FoodStage name={food3} image={Food3} description={description3}/>
+        <div className={`foodstage${isVisible ? ' visible' : ''}`}
+             style={isVisible ? {animationName: 'fadeInTranslate'} : {}}>
+            <div className='container'>
+                <FoodStage name={food1} image={Food1} description={description1}/>
+                <FoodStage name={food2} image={Food2} description={description2}/>
+                <FoodStage name={food3} image={Food3} description={description3}/>
+            </div>
         </div>
     )
 }
