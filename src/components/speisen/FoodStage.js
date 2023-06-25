922468f622bb7e119c6fc19ead085704ea937c79
lisakohls <lk210@hdm-stuttgart.de>
2023-06-25 22:33:50 +0200

    Neue Anordnung der Klassen und Componenten

--- src/components/speisen/FoodStage.js
+++ src/components/speisen/FoodStage.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import '../../styles/Food.css'
 import BlackFontText from '../text/BlackFontText'
 
 function FoodStage(props){
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const container = document.querySelector('.container');
+            const containerTop = container.getBoundingClientRect().top;
+            const windowHeight = window.innerHeight;
+
+            if (containerTop < windowHeight * 0.75) {
+                setIsVisible(true);
+            }
+            if (containerTop > windowHeight * 0.75) {
+                setIsVisible(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return(
+        <div className={`container${isVisible ? ' visible' : ''}`}>
         <div className="stage">
             <div className="circle-wrapper">
                 <img className="circle-image" src={props.image} alt={props.name} />
@@ -13,7 +35,7 @@ function FoodStage(props){
                 <BlackFontText text={props.description} formatierung='block'/>
             </div>
         </div>
-
+</div>
     )
 }
 
