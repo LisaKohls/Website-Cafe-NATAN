149c90e548681a5a07ce552119f0c66f2b92ee69
lisasalzer <ls228@hdm-stuttgart.de>
2023-06-29 15:15:14 +0200

    added Impressum.js and Datenschutz.js

--- src/components/Datenschutz.js
+++ src/components/Datenschutz.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "../styles/Impressum.css";
+import "../styles/Privacy.css";
 import {datenschutz} from "../resources/textInhalte";
 import WhiteFontText from "./text/WhiteFontText";
+import WhiteHeadlineSection from "./sections/WhiteHeadlineSection";
 
 function Datenschutz(){
     return(
-        <div className="datenschutz">
+        <div className="privacy">
             <div className="sectionPadding"/>
             <WhiteFontText text={datenschutz} formatierung='blocksatz'/>
         </div>
