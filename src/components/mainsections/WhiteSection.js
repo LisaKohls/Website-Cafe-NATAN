f0d797e4c55c8c5e87c92abf94091cf0961c0cce
lisasalzer <ls228@hdm-stuttgart.de>
2023-07-19 16:54:15 +0200

    changed size of partner logos

--- src/components/mainsections/WhiteSection.js
+++ src/components/mainsections/WhiteSection.js
@@ -8,19 +8,19 @@ import HeadlineSection from "../text/HeadlineSection";
 import Img1 from "../../resources/partnerLogos/Akis Getränke.png";
 import Img2 from "../../resources/partnerLogos/Biokiste Ortlieb.png";
 import Img3 from "../../resources/partnerLogos/Nast.png";
-import Img4 from "../../resources/partnerLogos/ginstr.jpg";
-import Img5 from "../../resources/partnerLogos/GMW Steuerberater.png";
-import Img6 from "../../resources/partnerLogos/DesignPlus.png";
-import Img7 from "../../resources/partnerLogos/gourmet-kochmanufaktur-logo.png";
+import Img4 from "../../resources/partnerLogos/ginstr_logo.png";
+import Img5 from "../../resources/partnerLogos/GMW_Steuerberater_Logo.png";
+import Img6 from "../../resources/partnerLogos/DesignPlus_Logo.png";
+import Img7 from "../../resources/partnerLogos/gourmet-kochmanufaktur_logo.png";
 import Img8 from "../../resources/partnerLogos/Karl_haidle_weingut_logo.svg.png";
 import Img9 from "../../resources/partnerLogos/kessler_logo.png";
-import Img10 from "../../resources/partnerLogos/Leonhard Stuttgart.png";
+import Img10 from "../../resources/partnerLogos/Leonhard-Stuttgart_Logo.png";
 import Img11 from "../../resources/partnerLogos/Mokuska.jpg";
-import Img12 from "../../resources/partnerLogos/the-english-tearoom-logo-black.png";
+import Img12 from "../../resources/partnerLogos/the-english-tearoom-logo.png";
 import Img13 from "../../resources/partnerLogos/Schönbuch.png";
 import Img14 from "../../resources/partnerLogos/English tearoom.jpg";
-import Img15 from "../../resources/partnerLogos/Der Blumenladen Stuttgart.png";
-import Img16 from '../../resources/partnerLogos/Porsche.png'
+import Img15 from "../../resources/partnerLogos/Der-Blumenladen-Stuttgart_Logo.png";
+import Img16 from '../../resources/partnerLogos/Porsche_Logo.png'
 import ScrollImages from "../scrollimages/ScrollImages";
 
 
