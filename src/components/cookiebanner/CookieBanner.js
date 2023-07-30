9f9b4d378100a19a2885889d112770158ddf1f67
lisasalzer <ls228@hdm-stuttgart.de>
2023-07-30 08:38:37 +0200

    fixed gallery for events

--- src/components/cookiebanner/CookieBanner.js
+++ src/components/cookiebanner/CookieBanner.js
@@ -1,15 +1,35 @@
-import React from 'react'
-import CookieConsent from "react-cookie-consent";
-import '../../styles/cookiebanner/CookieBanner.css'
+import React, { useState, useEffect } from 'react';
+import CookieConsent from 'react-cookie-consent';
+import '../../styles/cookiebanner/CookieBanner.css';
 
-function CookieBanner(){
-    return( <div>
-        <CookieConsent debug={true}
-                       className='banner'
-                       buttonStyle={{color: '#FFFFFF', background: '#808770', borderRadius:'10px', fontSize:'18px'}}
-                       buttonText='Verstanden!'>
-                        Diese Seite nutzt Cookies. Schau dir unsere <a href='/privacy' className='linkbanner'>Datenschutzerklärung</a> an für mehr Informationen</CookieConsent>
-    </div>)
+function CookieBanner() {
+    const [showBanner, setShowBanner] = useState(true);
+
+    useEffect(() => {
+        const hasConsent = localStorage.getItem('cookieConsent');
+        setShowBanner(!hasConsent);
+    }, []);
+
+    const handleAccept = () => {
+        localStorage.setItem('cookieConsent', 'true');
+        setShowBanner(false);
+    };
+
+    return (
+        showBanner && (
+            <div>
+                <CookieConsent
+                    debug={true}
+                    className="banner"
+                    buttonStyle={{ color: '#FFFFFF', background: '#808770', borderRadius: '10px', fontSize: '18px' }}
+                    buttonText="Verstanden!"
+                    onAccept={handleAccept}
+                >
+                    Diese Seite nutzt Cookies. Schau dir unsere <a href="/privacy" className="linkbanner">Datenschutzerklärung</a> an für mehr Informationen
+                </CookieConsent>
+            </div>
+        )
+    );
 }
 
-export default CookieBanner
+export default CookieBanner;
