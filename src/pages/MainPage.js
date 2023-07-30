4e3c9af837d49b9c096ead35c59cd41506a4cea3
lisasalzer <ls228@hdm-stuttgart.de>
2023-07-30 20:43:30 +0200

    Revert "added react helmet for SEO optimization"
    This reverts commit 17f5eb6b548a56f4a954e849e590e3c0d04caaee.

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -6,22 +6,12 @@ import WhiteSection from "../components/mainsections/WhiteSection";
 import HeadingSection from "../components/mainsections/HeadingSection";
 import Navbar from "../components/layout/Navbar";
 import GallerySection from "../components/mainsections/GallerySection";
-import { Helmet } from 'react-helmet-async';
-
 
 //the main page separates different sections of the website
 function MainPage() {
 
     return (
         <React.Fragment>
-            <Helmet>
-                <title>NATAN | CAFE AND BAR</title>
-                <meta name="description" content="Entdecken Sie moderne, vegetarische Küche und leckere Drinks im
-                Café NATAN – Genießen für den guten Zweck! Als Non-Profit-Bar fließt der gesamte Gewinn in humanitäre
-                Projekte. Mit einer vielfältigen Auswahl an Drinks und köstlichen veganen und vegetarischen Speisen
-                unterstützen wir weltweit Menschen in Not. Ein Team aus über 80 Volunteers ist mit Leidenschaft für
-                unsere Gäste da. Alle zusammen für die gute Sache!" />
-            </Helmet>
             <Navbar/>
             <StylePage bottomWheel={true}>
                 <HeadingSection/>
